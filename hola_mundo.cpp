06c9b676a2bfec5ef2a488ff9e54b724338096ae
Marielv Chiquinquira Mavarez Rangel <marielvmavarez@gmail.com>
2025-09-24 12:11:00 -0400

    feat: agregar mas mensajes al programa

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
     cout << "Mi nombre es: Marielv Mavarez" << endl;
+    cout<<"Estou usando Git en Clientes Web"<<endl;
+    cout<<"Este es mi segundo commit!"<<endl;
     return 0;
 }
